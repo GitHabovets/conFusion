c2a16a9b1d2b0a217550ffbefa36f8ede88eaeb7
Stan Siris <stan.siris@bnymellon.com>
2018-04-17 17:47:50 -0400

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,8 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from '../app/services/dish.service';
+import { PromotionService } from '../app/services/promotion.service';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
@@ -71,7 +73,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     //HttpModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
